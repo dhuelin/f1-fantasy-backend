daa7f7df8d3ce8c500721e395dd52030223a56f2
Denis <denis.huelin@gmail.com>
2025-01-17 15:58:05 +0100

    Setup Springboot Project

---
+++ src/main/java/com/dhuelin/f1/fantasy/backend/F1FantasyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dhuelin.f1.fantasy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1FantasyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1FantasyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhuelin/f1/fantasy/backend/F1FantasyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhuelin.f1.fantasy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1FantasyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
